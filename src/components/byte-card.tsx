277dae937a787faaa178fd937ffd76970218997c
Ismail Zabalawi <ismailzabalawi@gmail.com>
2025-07-06 22:35:04 +0000

    I believe that you can I.prove that and perfect it l, my artist

--- src/components/byte-card.tsx
+++ src/components/byte-card.tsx
@@ -6,7 +6,6 @@ import { Button } from "@/components/ui/button";
 import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { User, Heart, Bookmark, Link2, MessageCircle } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { Separator } from "./ui/separator";
 
 type LinkPreview = {
     title: string;
@@ -114,19 +113,18 @@ export default function ByteCard({ id, author, title, teret, imageUrl, link, lin
       
       <div className="flex-1" />
 
-      <CardFooter>
-        <div className="flex items-center gap-1">
+      <CardFooter className="flex justify-between">
+        <div className="flex items-center gap-2">
             <Button asChild variant="ghost" size="icon" className="rounded-full text-muted-foreground hover:text-primary">
               <Link href={`/byte/${id}`}><MessageCircle className="h-5 w-5" /></Link>
             </Button>
-            <Separator orientation="vertical" className="h-6 mx-1" />
             <Button variant="ghost" size="icon" className="rounded-full text-muted-foreground hover:text-primary">
                 <Heart className="h-5 w-5" />
             </Button>
-            <Button variant="ghost" size="icon" className="rounded-full text-muted-foreground hover:text-primary">
-                <Bookmark className="h-5 w-5" />
-            </Button>
         </div>
+        <Button variant="ghost" size="icon" className="rounded-full text-muted-foreground hover:text-primary">
+            <Bookmark className="h-5 w-5" />
+        </Button>
       </CardFooter>
     </Card>
   );
